e81e571ea979857451799f0b56af61d3f8368b76
Вельц Валентина <90384654+Vvelts@users.noreply.github.com>
2022-04-30 23:04:36 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Задание_4</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="text-dark">
+    <div class="container-fluid">
+        
+        <div class="items d-flex flex-column">
+            <div class="row d-flex flex-row justify-content-center mt-3 order-sm-3">
+                <div class="col-sm-9 content">
+                    <div class="items d-flex flex-column ">
+                        <div id="form" class="order-sm-3">
+                            <?php
+    
+                                if (!empty($messages)) {
+                                    print('<div id="messages">');
+                                    foreach ($messages as $message) {
+                                        print($message);
+                                    }
+                                    print('</div>');
+                                }
+                            ?>
+                             <h2 class="text-center">Форма</h2>
+                              <form action="" method="POST">
+                                    <label>
+                                        Имя:<br />
+                                        <input name="name" <?php if ($errors['name'] || $errors['1']) {print "class='error'" ;} ?> value="<?php print $values['name']; ?>" />
+                                    </label><br />
+    
+                                    <label>
+                                        E-mail:<br />
+                                        <input name="email" <?php if ($errors['email'] || $errors['2']) {print 'class="error"' ;} ?> value="<?php print $values['email']; ?>"  />
+                                    </label><br />
+    
+                                    <label>
+                                        День рождения:<br />
+                                        <input name="date" type="date" <?php if ($errors['date'] || $errors['3']) {print 'class="error"' ;} ?> value="<?php print $values['date']; ?>" />
+                                    </label><br />
+                                    Пол:<br />
+                                    <label><input type="radio" name="radio-group-1" <?php if ($errors['radio-group-1']) {print 'class="error"' ;} if($values['radio-group-1']=="М"){print "checked='checked'";}?> value="М" />
+                                        Муж.</label>
+                                    <label><input type="radio" name="radio-group-1" <?php if ($errors['radio-group-1']) {print 'class="error"' ;} if($values['radio-group-1']=="Ж"){print "checked='checked'";}?> value="Ж" />
+                                        Жен</label><br />
+                                    Кол-во конечностей:<br />
+                                    <label><input type="radio" name="radio-group-2" <?php if ($errors['radio-group-2']) {print 'class="error"' ;} if($values['radio-group-2']=="4"){print "checked='checked'";}?> value="1" />
+                                        1</label>
+                                    <label><input type="radio" name="radio-group-2" <?php if ($errors['radio-group-2']) {print 'class="error"' ;} if($values['radio-group-2']=="5"){print "checked='checked'";}?> value="2" />
+                                        2</label>
+                                    <label><input type="radio" name="radio-group-2" <?php if ($errors['radio-group-2']) {print 'class="error"' ;} if($values['radio-group-2']=="6"){print "checked='checked'";}?> value="3" />
+                                        3</label>
+                                    <label><input type="radio" name="radio-group-2" <?php if ($errors['radio-group-2']) {print 'class="error"' ;} if($values['radio-group-2']=="7"){print "checked='checked'";}?> value="4" />
+                                        4</label><br />    
+                                        
+                                    <label>
+                                        Сверхспособности:
+                                        <br />
+                                        <select name="superpowers" multiple="multiple">
+                                        <option value="Бессмертие" <?php if($values['superpowers']=="Бессмертие"){print "selected='selected'";}?>>Бессмертие</option>
+                                        <option value="Левитация" <?php if($values['superpowers']=="Левитация"){print "selected='selected'";}?>>Левитация</option>
+                                        <option value="Прохождение сквозь стены" <?php if($values['superpowers']=="Прохождение сквозь стены"){print "selected='selected'";}?>>Прохождение сквозь стены</option>
+                                        </select>
+                                    </label><br />
+                                    <label>
+                                        Биография:<br />
+                                        <textarea name="biography" <?php if ($errors['biography']) {print 'class="error"' ;} ?>><?php print $values['biography']; ?></textarea> 
+                                    </label><br />
+                                    Ознакомлен с условиями:<br />
+                                    <label><input type="checkbox" name="check-1" <?php  if($values['check-1']=="1"){print "checked='checked'";}?> value="1" />
+                                        Согласен с условиями</label><br /> 
+                                    <input type="submit" value="Отправить" />
+                                </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,201 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  $messages = array();
+	if (!empty($_COOKIE['save'])) {
+      setcookie('save', '', 100000);
+    	$messages[] = 'Данные сохранены';
+  	}
+    $errors = array();
+    $errors['name'] = !empty($_COOKIE['name_error']);
+    $errors['email'] = !empty($_COOKIE['email_error']);
+    $errors['date'] = !empty($_COOKIE['date_error']);
+    $errors['radio-group-1'] = !empty($_COOKIE['radio-group-1_error']);
+    $errors['radio-group-2'] = !empty($_COOKIE['radio-group-2_error']);
+    $errors['superpowers'] = !empty($_COOKIE['superpowers_error']);
+    $errors['biography'] = !empty($_COOKIE['biography_error']);
+    $errors['check-1'] = !empty($_COOKIE['check-1_error']);
+    $errors['1'] = !empty($_COOKIE['1_error']);
+    $errors['2'] = !empty($_COOKIE['2_error']);
+    $errors['3'] = !empty($_COOKIE['3_error']);
+
+    if ($errors['name']) {
+      setcookie('name_error', '', 100000);
+      $messages[] = '<div class="error">Введите имя.</div>';
+    }
+    if ($errors['1']){
+      setcookie('1_error', '', 100000);
+      $messages[] = '<div class="error">Введите имя латинскими буквами</div>';
+    } 
+    if ($errors['email']) {
+      setcookie('email_error', '', 100000);
+      $messages[] = '<div class="error">Введите email.</div>';
+    }
+    if ($errors['2']){
+      setcookie('2_error', '', 100000);
+      $messages[] = '<div class="error">email имеет вид: test@example.com</div>';
+    } 
+    if ($errors['date']) {
+      setcookie('date_error', '', 100000);
+      $messages[] = '<div class="error">Введите дату рождения.</div>';
+    }
+    if ($errors['3']){
+      setcookie('3_error', '', 100000);
+      $messages[] = '<div class="error">Формат даты 19.09.2000</div>';
+    } 
+    if ($errors['radio-group-1']) {
+      setcookie('radio-group-1_error', '', 100000);
+      $messages[] = '<div class="error">Укажите пол.</div>';
+    }
+    if ($errors['radio-group-2']) {
+      setcookie('radio-group-2_error', '', 100000);
+      $messages[] = '<div class="error">Укажите кол-во конечностей.</div>';
+    }
+    if ($errors['superpowers']) {
+      setcookie('superpowers_error', '', 100000);
+      $messages[] = '<div class="error">Укажите суперспособность.</div>';
+    }
+    if ($errors['biography']) {
+      setcookie('biography_error', '', 100000);
+      $messages[] = '<div class="error">Напишите биографию.</div>';
+    }
+    if ($errors['check-1']) {
+      setcookie('check-1_error', '', 100000);
+      $messages[] = '<div class="error">Примите условия.</div>';
+    }
+  
+    $values = array();
+
+    $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+    $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+    $values['date'] = empty($_COOKIE['date_value']) ? '' : $_COOKIE['date_value'];
+    $values['radio-group-1'] = empty($_COOKIE['radio-group-1_value']) ? '' : $_COOKIE['radio-group-1_value'];
+    $values['radio-group-2'] = empty($_COOKIE['radio-group-2_value']) ? '' : $_COOKIE['radio-group-2_value'];
+    $values['superpowers'] = empty($_COOKIE['superpowers_value']) ? '' : $_COOKIE['superpowers_value'];
+    $values['biography'] = empty($_COOKIE['biography_value']) ? '' : $_COOKIE['biography_value'];
+    $values['check-1'] = empty($_COOKIE['check-1_value']) ? '' : $_COOKIE['check-1_value'];
+  
+
+  	include('form.php');
+}
+else{
+
+  $errors = FALSE;
+
+  if (!preg_match("/^[-a-zA-Z]+$/",$_POST['name'])){
+    setcookie('1_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } 
+  if (!preg_match("/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/",$_POST['email'])){
+    setcookie('2_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  } 
+ 
+  if (empty($_POST['name'])) {
+    setcookie('name_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('name_value', $_POST['name'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['email'])) {
+    setcookie('email_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('email_value', $_POST['email'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['date'])) {
+    setcookie('date_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('date_value', $_POST['date'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['radio-group-1'])) {
+    setcookie('radio-group-1_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('radio-group-1_value', $_POST['radio-group-1'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['radio-group-2'])) {
+    setcookie('radio-group-2_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('radio-group-2_value', $_POST['radio-group-2'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['superpowers'])) {
+    setcookie('superpowers_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('superpowers_value', $_POST['superpowers'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['biography'])) {
+    setcookie('biography_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('biography_value', $_POST['biography'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['check-1'])) {
+    setcookie('check-1_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('check-1_value', $_POST['check-1'], time() + 365 * 24 * 60 * 60);
+  }
+
+  if ($errors) {
+    header('Location: index.php');
+    exit();
+  }
+  else {
+    setcookie('name_error', '', 100000);
+    setcookie('1_error', '', 100000);
+    setcookie('email_error', '', 100000);
+    setcookie('2_error', '', 100000);
+    setcookie('date_error', '', 100000);
+    setcookie('3_error', '', 100000);
+    setcookie('radio-group-1_error', '', 100000);
+    setcookie('radio-group-2_error', '', 100000);
+    setcookie('superpowers_error', '', 100000);
+    setcookie('biography_error', '', 100000);
+    setcookie('check-1_error', '', 100000);
+  }
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = $_POST['date'];
+    $gender = $_POST['radio-group-1'];
+    $limbs = $_POST['radio-group-2'];
+    $powers = $_POST['superpowers'];
+    $policy = $_POST['check-1'];
+
+
+  
+    $user = 'u47507';
+    $pass = '2613634';
+    $db = new PDO('mysql:host=localhost;dbname=u47507', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+    // var_dump($name ." ". $email ." ". $date ." ". $gender ." ". $limbs ." ". $policy ." ". $powers);
+    // exit();
+    try {
+        $stmt = $db->prepare("INSERT INTO users SET name = ?, email = ?, date = ?, gender = ?, limbs = ?, policy = ?");
+        $stmt -> execute(array($name, $email, $date, $gender, $limbs, $policy));
+        $power_id = $db->lastInsertId();
+        
+        $superpowers = $db->prepare("INSERT INTO powers SET power_id = ?, powers = ?");
+        $superpowers -> execute(array($power_id, $powers));
+      }
+      catch(PDOException $e){
+        print('Error : ' . $e->getMessage());
+        exit();
+      }
+
+    
+    setcookie('save', '1');
+    header('Location: index.php');
+}
